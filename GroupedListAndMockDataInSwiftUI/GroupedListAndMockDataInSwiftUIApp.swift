c84b14dda993188e948155e62238dd1748af70fe
Ramill <ramill_ibragimov@icloud.com>
2022-08-01 12:47:46 +0300

    Initial Commit

---
+++ GroupedListAndMockDataInSwiftUI/GroupedListAndMockDataInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupedListAndMockDataInSwiftUIApp.swift
+//  GroupedListAndMockDataInSwiftUI
+//
+//  Created by Ramill Ibragimov on 01.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct GroupedListAndMockDataInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
